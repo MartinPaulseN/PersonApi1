0ef930921cd03112293f5a1959eaab7633453554
ITHS <martinpaulsen617@gmail.com>
2025-04-29 01:30:27 +0200

    Skapat Person filen

--- src/main/java/org/example/personapi1/PersonApi1Application.java
+++ src/main/java/org/example/personapi1/PersonApi1Application.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PersonApi1Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PersonApi1Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PersonApi1Application.class, args);
+    }
 
 }
